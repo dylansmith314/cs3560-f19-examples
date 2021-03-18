2e1a130cfeb61cecec4dd793fbe4e629090dd288
JoshMencsik <jm062718@ohio.edu>
2021-03-18 10:58:22 -0400

    Reformatted missing_numbers using psf/black

--- missing_numbers/missing_numbers.py
+++ missing_numbers/missing_numbers.py
@@ -1,12 +1,19 @@
 """Python code solution to "Missing Number" Kata on codewars"""
+
+
 def missing_number(x):
-    if x == 0: return 2
-    if x == 1: return 5
-    if x == 2: return 35
-    if x == 3: return 282
-    if x == 4: return 2600
+    if x == 0:
+        return 2
+    if x == 1:
+        return 5
+    if x == 2:
+        return 35
+    if x == 3:
+        return 282
+    if x == 4:
+        return 2600
     base = "31622776601683793319988935444327185337195551393252168268575048527925944386392382213442481083793002951873472841528400551485488560304538800146905195967001539033449216571792599406591501534741133394841240853169295770904715764610443692578790620379"
-    if x%2 == 0:
-        return int("25" + "0"*(x//2-3)+"1000"+"0"*(x//2-3))
+    if x % 2 == 0:
+        return int("25" + "0" * (x // 2 - 3) + "1000" + "0" * (x // 2 - 3))
     else:
-        return int("25" + "0"*(x//2-2)+str(int(base[0:3+(x-5)//2])+1))
+        return int("25" + "0" * (x // 2 - 2) + str(int(base[0 : 3 + (x - 5) // 2]) + 1))
--- missing_numbers/missingnum.py
+++ missing_numbers/missingnum.py
@@ -1,10 +1,14 @@
 # code for codewars problem: Missing Numbers
 from math import isqrt
+
+
 def missing_number(n):
-    if n == 0: return 2
-    p10 = 10**n
-    if ~n&1:
-        return (p10//2+10**(n//2)-1)**2//p10+1
-    res = p10//2+ isqrt(p10)
-    if res**2//p10 - (res-1)**2//p10 <=1: res+=1
-    return (res-1)**2//p10+1
+    if n == 0:
+        return 2
+    p10 = 10 ** n
+    if ~n & 1:
+        return (p10 // 2 + 10 ** (n // 2) - 1) ** 2 // p10 + 1
+    res = p10 // 2 + isqrt(p10)
+    if res ** 2 // p10 - (res - 1) ** 2 // p10 <= 1:
+        res += 1
+    return (res - 1) ** 2 // p10 + 1

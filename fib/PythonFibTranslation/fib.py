582e58ccaee383df976e2189d8a1b57e5b010329
JoshMencsik <jm062718@ohio.edu>
2021-03-18 13:07:39 -0400

    Ran psf/black on PythonFibTranslation

--- fib/PythonFibTranslation/fib.py
+++ fib/PythonFibTranslation/fib.py
@@ -3,17 +3,20 @@
 @author Johnny Gilbert
 @brief This file contains both a recursive and iterative version of calculating fibonacci combinations
 """
+
+
 def fib_recursive(n):
-    if (n == 1 or n == 2):
+    if n == 1 or n == 2:
         return 1
     return fib_recursive(n - 1) + fib_recursive(n - 2)
+
+
 def fib_iterative(n):
     temp = 0
     a = 1
     b = 1
 
-   
-    if (n == 1 or n == 2):
+    if n == 1 or n == 2:
         return 1
 
     for x in range(2, n):
